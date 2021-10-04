43a2aa2f5e8b42ffadab0a2514922b7ef49aa3f1
vishaln2425 <vishaln2425@gmail.com>
2021-10-04 20:13:56 +0530

    change

--- Session 4/src/com/company/Main.java
+++ Session 4/src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	boolean gameOver = true;
+	int score = 5000;
+	int levelCompleted = 5;
     }
 }
